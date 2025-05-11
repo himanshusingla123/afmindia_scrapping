8d206aadef104f614eb746da18aaa5b1c430fd8c
Himanshu Singla <95504579+himanshusingla123@users.noreply.github.com>
2025-05-11 10:02:28 +0530

    Update extract_nav.sh

--- extract_nav.sh
+++ extract_nav.sh
@@ -1,5 +1,8 @@
+# this is used for download the file and name it as NAVAll.txt
 curl -s https://www.amfiindia.com/spages/NAVAll.txt -o NAVAll.txt
 
+# this is to extract the meaningful insights like Schema name and Net Asset Value from file and save into nav_data.tsv
 awk -F ';' 'NF > 1 && $1 != "Scheme Code" { print $4 "\t" $5 }' NAVAll.txt > nav_data.tsv
 
+#after doing all the above tasks it prints ...
 echo "Extraction complete. Data saved to nav_data.tsv"
